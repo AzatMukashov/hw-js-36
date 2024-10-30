1de1ee9cf3f1b284a1b7f8e81916fd1d587a0c58
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-10-30 21:47:56 +0600

    вынес навигацию в компонент, "расфасовал" компоненты по их смысловой нагрузке)

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
-import { BrowserRouter as Router, Route, Routes,  NavLink } from 'react-router-dom';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import Page from './components/Page/Page.tsx';
-import Admin from './components/Admin/Admin.tsx';
+import Admin from './containers/Admin/Admin.tsx';
+import ToolBar from './components/ToolBar/ToolBar.tsx';
 
 const App = () => {
   return (
     <Router>
       <div>
-        <Navbar bg="primary" variant="dark" expand="lg">
-          <Container>
-            <Navbar.Brand href="/">Static Pages</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ms-auto">
-                <Nav.Link as={NavLink} to="/pages/about" end>About</Nav.Link>
-                <Nav.Link as={NavLink} to="/pages/contacts" end>Contacts</Nav.Link>
-                <Nav.Link as={NavLink} to="/pages/services" end>Services</Nav.Link>
-                <Nav.Link as={NavLink} to="/pages/faq" end>FAQ</Nav.Link>
-                <Nav.Link as={NavLink} to="/pages/blog" end>Blog</Nav.Link>
-                <Nav.Link as={NavLink} to="/pages/admin" end>Admin</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+        <ToolBar/>
         <Container className="mt-4">
           <Routes>
             <Route path="/" element={<div>Welcome to the Website</div>}/>
